eade5e2ab0eadd93146264de76db4a59071e20fe
Sean O'Connor <oconnorjamessean@gmail.com>
2018-04-10 16:09:46 +0100

    Initial Commit

---
+++ Health Application/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Health Application
+//
+//  Created by Sean O'Connor on 10/04/2018.
+//  Copyright © 2018 Sean O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
